991e1c9be07190206238908a5b6c66789c23daaf
Esteban Cruz/Other/Consultant <cruesteb@mexcruesteb4120.local>
2020-10-08 19:25:59 -0500

    update

--- assets/js/primary.js
+++ assets/js/primary.js
@@ -8,11 +8,11 @@ $(document).ready(function(){
         $(".av-title-container").css("backgroundPosition","center");
        }
     });
-    $('.av-controls').click(function(){
-        let avPlaceHolder = $(this).find('label').text();
-        $(this).find('input').focus().attr('placeholder', ''+avPlaceHolder+'').css('borderBottomColor','#7F28C4');
-        $(this).find('.sc-formfield-label').addClass('activeLabel');
-    });
-    $('.section').prepend('<h3 class="av-form-title">Registro al concierto Avon</h3>');
+    alert();
+    // $('.av-controls').click(function(){
+    //     let avPlaceHolder = $(this).find('label').text();
+    //     $(this).find('input').focus().attr('placeholder', ''+avPlaceHolder+'').css('borderBottomColor','#7F28C4');
+    //     $(this).find('.sc-formfield-label').addClass('activeLabel');
+    // });
 
 });
