f685097d6045916090c2015976af5b6760ffcdbb
Esteban Cruz/Other/Consultant <cruesteb@mexcruesteb4120.local>
2021-03-01 08:31:56 -0600

    Update

--- assets/js/avSlider.js
+++ assets/js/avSlider.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
     var slideIndex = 2;
     var timeOut;
 
--- assets/js/repHomeOverlay.js
+++ assets/js/repHomeOverlay.js
@@ -1,5 +1,12 @@
-const avFreshConfig = [
-    
+
+
+
+const avModalSwitch = true;
+// Asignar a false si se quiere descativar el modal
+
+//Las imagenes se colocan en esta carpeta /REPSuite/static/images/home/
+
+const avFreshConfig = [    
     {
         link:'/REPSuite/interfaz.page',
         img: 'avModalTest1.jpg',
@@ -11,6 +18,8 @@ const avFreshConfig = [
         title: 'Aprende Tu espacio Avon'
     },
 ];
+
+// Repres despues del loa 7
 const avOlderConfig = [
     {
         link: '/REPSuite/interfaz.page',
@@ -28,66 +37,62 @@ const avOlderConfig = [
 
 
 
-var avPrinter,avConfig,loaCheck;
-avPrinter = ['']
-//var loaCheck = AvonAnalyticsObjex.Profile.loa;
-var loaCheck = 9;
-if (loaCheck < 7){
-    avConfig = avFreshConfig;
-} else {
-    avConfig = avOlderConfig;  
-}
-for (i=0;i < avConfig.length;i++) {
-    avPrinter += '<div class="avModalButton" av-data="'+ i +'">'+ avConfig[i].title +'</div>'
-};
 
-var avModalhtml = '<div id="avOverlay" class="av-modal">'+
-            '<div class="av-modal-background avClose" onclick="avClose()"></div>'+
-            '<div style="z-index: 9999;">'+
-            '<div class="av-modal-content">'+
-            '<div class="av-modal-close avClose" onclick="avClose()">&#10005;</div>'+
-            '<div class="av-modal-hcontainer"><div class="av-modal-hside">'+
-            '<div class="av-wrapper modalFade"><img src="/REPSuite/static/images/home/brand.jpg" alt="AVON">'+
-            '<h2 class="av-title">Bienvenida</h2><h2 class="av-title" id="avModalName"></h2></div>'+
-            '<div class="av-wrapper av-modal-hbuttons"><p class="av-text">Tenemos noticias para ti:</p>'+ avPrinter +
-            '<a class="av-button-secondary" href="/REPSuite/PlaceAnOrder.page" target="_top">Colocar una orden</a></div></div>'+
-            '<div class="av-modal-himage"><a id="avLinkModal" target="_top" href="'+ avConfig[0].link +'" >'+
-            '<div id="avImageModal" style="background-image:url(\'/REPSuite/static/images/home/'+ avConfig[0].img +'\');" class="av-modal-himg"></div></a>'+
-            '</div></div></div></div></div>';
+if (avModalSwitch){
+    var avPrinter,avConfig,loaCheck;
+    avPrinter = ['']
+    //var loaCheck = AvonAnalyticsObjex.Profile.loa;
+    var loaCheck = 9;
+    if (loaCheck < 7){
+        avConfig = avFreshConfig;
+    } else {
+        avConfig = avOlderConfig;  
+    }
+    for (i=0;i < avConfig.length;i++) {
+        avPrinter += '<div class="avModalButton" av-data="'+ i +'">'+ avConfig[i].title +'</div>'
+    };
+    
+    var avModalhtml = '<div id="avOverlay" class="av-modal">'+
+                '<div class="av-modal-background avClose"></div>'+
+                '<div style="z-index: 9999;">'+
+                '<div class="av-modal-content">'+
+                '<div class="av-modal-close avClose">&#10005;</div>'+
+                '<div class="av-modal-hcontainer"><div class="av-modal-hside">'+
+                '<div class="av-wrapper modalFade"><img src="/REPSuite/static/images/home/brand.jpg" alt="AVON">'+
+                '<h2 class="tbl_header_big" style="margin-bottom:10px;">Bienvenida</h2><h2 class="tbl_header_big" id="avModalName"></h2></div>'+
+                '<div class="av-wrapper av-modal-hbuttons"><p class="sub_heading" style="margin-bottom:14px;">Tenemos noticias para ti:</p>'+ avPrinter +
+                '<a class="av-button-secondary" id="#modalPnO" href="/REPSuite/PlaceAnOrder.page" target="_top">Colocar una orden</a></div></div>'+
+                '<div class="av-modal-himage"><a id="avLinkModal" target="_blank" href="'+ avConfig[0].link +'" >'+
+                '<div id="avImageModal" style="background-image:url(\'/REPSuite/static/images/home/'+ avConfig[0].img +'\');" class="av-modal-himg"></div></a>'+
+                '</div></div></div></div></div>';
                 
-if (sessionStorage.getItem("homeModalClosed") != "true") {
-    $('body').append(avModalhtml);
-};
+                
+    if (sessionStorage.getItem("homeModalClosed") != "true") {
+        var avContainer = document.createElement('div');
+        avContainer.innerHTML = avModalhtml;
+        var avTarget = parent.document.querySelector('body');
+        avTarget.parentNode.insertBefore(avContainer,avTarget.nextSibling);
+    };
+}
+
 
 $(document).ready(function(){
-    let avModalNameGet = $('#avModalNameGet').text();
-    $('#avModalName').text(avModalNameGet);
-    $(".avModalButton").click(function () {
-        let avData = $(this).attr('av-data');
-        $(this).addClass('active').siblings().removeClass('active');
-        $('#avImageModal').attr('style', 'background-image:url(\'/REPSuite/static/images/home/'+ avConfig[avData].img+'\');')
-        $('#avLinkModal').attr('href', avConfig[avData].link)
-    });
-    $(".avClose").click(function () {
-        $('#avOverlay').hide();
-        sessionStorage.setItem("homeModalClosed", "true");
-    });
-    $(".avClose").click(function () {
-        $('#avOverlay').hide();
-        sessionStorage.setItem("homeModalClosed", "true");
-    });
-    let avMoreTitle,avMore;
-    $('.avMore-open').click(function(){
-        avMoreTitle = $(this).attr('av-data');
-        avMore = $(this).attr('av-more');
-        $('#avMoreId').addClass('active');
-        $('#avMore-title').text(avMoreTitle);
-        $('.avMore-'+ avMore).show(function(){
-            $(this).siblings().hide();
+    if (avModalSwitch){
+        
+        let avModalNameGet = $('#avModalNameGet').text();
+        $('.avModalButton',parent.document).eq(0).addClass('active');
+        parent.document.querySelector('#avModalName').textContent = avModalNameGet;
+        $(".avClose, #modalPnO",parent.document).click(function(){
+            $("#avOverlay",parent.document).hide();
+            sessionStorage.setItem("homeModalClosed", "true");
         });
-    });
+        
+        $(".avModalButton",parent.document).click(function () {
+            let avData = $(this).attr('av-data');
+            $(this).addClass('active').siblings().removeClass('active');
+            $('#avImageModal',parent.document).attr('style', 'background-image:url(\'/REPSuite/static/images/home/'+ avConfig[avData].img+'\');')
+            $('#avLinkModal',parent.document).attr('href', avConfig[avData].link)
+        });    
     
-    $('.avMore-close').click(function(){
-        $('#avMoreId').removeClass('active');
-    });
+    }
 });
