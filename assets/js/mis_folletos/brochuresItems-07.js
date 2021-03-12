1deaf24b050b13bb9ba1fe9949a173238bebcebf
Esteban Cruz/Other/Consultant <cruesteb@mexcruesteb4120.local>
2021-03-11 21:43:17 -0600

    Update

--- assets/js/mis_folletos/brochuresItems-07.js
+++ assets/js/mis_folletos/brochuresItems-07.js
@@ -1,24 +1,49 @@
 
 const brochures7 = [
     {
-        title: 'Avon Contigo 3',
-        file: 'avon_contigo_c0121.pdf',
+        title: 'Minifolleto de las mejores marcas',
+        file: 'MinifolletoMejoresMarcas.pdf',
+        type: 'flyer',
+    },
+    {
+        title: 'Flyer Empresarias Día de las madres',
+        file: 'FlyerEmpresariasMadres.pdf',
+        type: 'flyer',
+    },
+    {
+        title: 'Infografía Día de las madres',
+        file: 'InfografiaMadres.pdf',
+        type: 'flyer',
+    },
+    {
+        title: 'Invitación Día de las madres',
+        file: 'InvitacionMadres.pdf',
+        type: 'flyer',
+    },
+    {
+        title: 'Minifolleto blancos 2q 2021',
+        file: 'MinifolletoBcos2q2021.pdf',
+        type: 'flyer',
+    },
+    {
+        title: 'Minifolleto de las mejores marcas',
+        file: 'MinifolletoMejoresMarcas.pdf',
+        type: 'flyer',
+    },
+    {
+        title: 'Minifolleto preventa Día de las madres',
+        file: 'MinifolletoPreventaMadres.pdf',
+        type: 'flyer',
+    },
+    {
+        title: 'Resumen de pedido Día de las madres',
+        file: 'ResumenpedidoDiaMadres.pdf',
+        type: 'flyer',
+    },
+    {
+        title: 'Avon Contigo',
+        file: 'avon_contigo_c0721.pdf',
         type: 'contigo',
-    },
-    {
-        title: 'Belleza',
-        file: '1',
-        type: 'imb',
-    },
-    {
-        title: 'Fashion & Home',
-        file: '2',
-        type: 'imb',
-    },
-    {
-        title: 'Rutinas Anew',
-        file: '3',
-        type: 'imb',
     },
 ]
 
--- assets/js/repHomeOverlay.js
+++ assets/js/repHomeOverlay.js
@@ -1,98 +1,94 @@
 
-
-
-const avModalSwitch = true;
-// Asignar a false si se quiere descativar el modal
-
-//Las imagenes se colocan en esta carpeta /REPSuite/static/images/home/
-
-const avFreshConfig = [    
+// Image route:  "/REPSuite/static/images/home/"
+// Content for new REPs LOA < 7
+const avFreshConfig = [
+    
     {
         link:'/REPSuite/interfaz.page',
-        img: 'avModalTest1.jpg',
-        title: 'Nueva interfaz'
+        img: 'overlayBienvenida.jpg',
+        title: 'Tu nuevo catalgo'
     },
     {
         link: '/REPSuite/Aprende.page',
-        img: 'avModalTest2.jpg',
-        title: 'Aprende Tu espacio Avon'
+        img: 'overlayBienvenida2.jpg',
+        title: 'Tu opinión vale'
     },
 ];
 
-// Repres despues del loa 7
+// Only one working at the moment
+// Content for older REPs LOA >= 7
 const avOlderConfig = [
     {
-        link: '/REPSuite/interfaz.page',
-        img: 'avModalTest1.jpg',
-        title: 'Nueva interfaz'
+        link:'/REPSuite/vendorLinksOne.page?linkid=42',
+        img: 'overlayBienvenida.jpg',
+        title: 'Tu nuevo catálogo'
     },
     {
-        link: '/REPSuite/Encuesta.page',
-        img: 'avModalTest2.jpg',
-        title: 'Encuesta'
+        link: 'https://c5afw467.caspio.com/dp/c022600001822daaa18543e3a224',
+        img: 'overlayBienvenida2.jpg',
+        title: 'Tu opinión nos interesa'
     },
 ];
 
+// NCM PUSH
+avNCMunshift(AvonAnalyticsObjex.Profile.repAcct);
 
 
-
-
-
-if (avModalSwitch){
-    var avPrinter,avConfig,loaCheck;
-    avPrinter = ['']
-    //var loaCheck = AvonAnalyticsObjex.Profile.loa;
-    var loaCheck = 9;
-    if (loaCheck < 7){
-        avConfig = avFreshConfig;
-    } else {
-        avConfig = avOlderConfig;  
-    }
-    for (i=0;i < avConfig.length;i++) {
-        avPrinter += '<div class="avModalButton" av-data="'+ i +'">'+ avConfig[i].title +'</div>'
-    };
-    
-    var avModalhtml = '<div id="avOverlay" class="av-modal">'+
-                '<div class="av-modal-background avClose"></div>'+
-                '<div style="z-index: 9999;">'+
-                '<div class="av-modal-content">'+
-                '<div class="av-modal-close avClose">&#10005;</div>'+
-                '<div class="av-modal-hcontainer"><div class="av-modal-hside">'+
-                '<div class="av-wrapper modalFade"><img src="/REPSuite/static/images/home/brand.jpg" alt="AVON">'+
-                '<h2 class="tbl_header_big" style="margin-bottom:10px;">Bienvenida</h2><h2 class="tbl_header_big" id="avModalName"></h2></div>'+
-                '<div class="av-wrapper av-modal-hbuttons"><p class="sub_heading" style="margin-bottom:14px;">Tenemos noticias para ti:</p>'+ avPrinter +
-                '<a class="av-button-secondary" id="#modalPnO" href="/REPSuite/PlaceAnOrder.page" target="_top">Colocar una orden</a></div></div>'+
-                '<div class="av-modal-himage"><a id="avLinkModal" target="_blank" href="'+ avConfig[0].link +'" >'+
-                '<div id="avImageModal" style="background-image:url(\'/REPSuite/static/images/home/'+ avConfig[0].img +'\');" class="av-modal-himg"></div></a>'+
-                '</div></div></div></div></div>';
-                
-                
-    if (sessionStorage.getItem("homeModalClosed") != "true") {
-        var avContainer = document.createElement('div');
-        avContainer.innerHTML = avModalhtml;
-        var avTarget = parent.document.querySelector('body');
-        avTarget.parentNode.insertBefore(avContainer,avTarget.nextSibling);
-    };
+var avPrinter,avConfig,loaCheck,avdownload;
+avPrinter = ['']
+//var loaCheck = AvonAnalyticsObjex.Profile.loa;
+var loaCheck = 9;
+if (loaCheck < 7){
+    avConfig = avFreshConfig;
+} else {
+    avConfig = avOlderConfig;
 }
+for (i=0;i < avConfig.length;i++) {
+    avPrinter += '<div class="avModalButton" av-data="'+ i +'">'+ avConfig[i].title +'</div>'
+};
 
+
+var avModalhtml = '<div id="avOverlay" class="av-modal">'+
+            '<div class="av-modal-background avClose" onclick="avClose()"></div>'+
+            '<div style="z-index: 9999;">'+
+            '<div class="av-modal-content">'+
+            '<div class="av-modal-close avClose" onclick="avClose()">&#10005;</div>'+
+            '<div class="av-modal-hcontainer"><div class="av-modal-hside">'+
+            '<div class="av-wrapper modalFade"><img src="/REPSuite/static/images/home/brand.jpg" alt="AVON">'+
+            '<h2 class="av-title my-2">Bienvenida</h2><h2 class="av-title" id="avName"></h2></div>'+
+            '<div class="av-wrapper av-modal-hbuttons"><p class="av-text">Tenemos noticias para ti:</p>'+ avPrinter +
+            '<a class="av-button-secondary" href="/REPSuite/PlaceAnOrder.page" target="_top">Colocar una orden</a></div></div>'+
+            '<div class="av-modal-himage"><a id="avLinkModal" target="_blank" href="'+ avConfig[0].link +'" >'+
+            '<div id="avImageModal" style="background-image:url(\'/REPSuite/static/images/home/'+ avConfig[0].img +'\');" class="av-modal-himg"></div></a>'+
+            '</div></div></div></div></div>';
+                
+if (sessionStorage.getItem("homeModalClosed") != "true") {
+    $('body').append(avModalhtml);
+};
 
 $(document).ready(function(){
-    if (avModalSwitch){
-        
-        let avModalNameGet = $('#avModalNameGet').text();
-        $('.avModalButton',parent.document).eq(0).addClass('active');
-        parent.document.querySelector('#avModalName').textContent = avModalNameGet;
-        $(".avClose, #modalPnO",parent.document).click(function(){
-            $("#avOverlay",parent.document).hide();
-            sessionStorage.setItem("homeModalClosed", "true");
-        });
-        
-        $(".avModalButton",parent.document).click(function () {
-            let avData = $(this).attr('av-data');
-            $(this).addClass('active').siblings().removeClass('active');
-            $('#avImageModal',parent.document).attr('style', 'background-image:url(\'/REPSuite/static/images/home/'+ avConfig[avData].img+'\');')
-            $('#avLinkModal',parent.document).attr('href', avConfig[avData].link)
-        });    
-    
+    let avName = $('#avNameGet').text();
+    $('#avName').text(avName);
+    $('.avModalButton').eq(0).addClass('active');
+    $(".avModalButton").click(function () {
+        let avData = $(this).attr('av-data');
+        $(this).addClass('active').siblings().removeClass('active');
+        $('#avImageModal').attr('style', 'background-image:url(\'/REPSuite/static/images/home/'+ avConfig[avData].img+'\');')
+        $('#avLinkModal').attr('href', avConfig[avData].link)
+        if (avdownload) {
+            if (avData == 0 || avData == 1){
+                $('#avLinkModal').attr('download', 'Notificación de Cambio AVON')
+            }
+        } else {
+            $('#avLinkModal').removeAttr('download');
+        }
+    });
+    if (avdownload) {
+        $('#avLinkModal').attr('download', 'Notificación de Cambio AVON')
     }
+    $(".avClose").click(function () {
+        $('#avOverlay').hide();
+        sessionStorage.setItem("homeModalClosed", "true");
+    });
+    
 });
