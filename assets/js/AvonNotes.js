05d608f29af4ee12ca0cf793b7db7691e2017eff
Esteban Cruz/Other/Consultant <cruesteb@mexcruesteb4120.local>
2021-09-14 13:05:57 -0500

    Anew updates

--- assets/js/AvonNotes.js
+++ assets/js/AvonNotes.js
@@ -1,256 +1,462 @@
-let avData = [
+const imssSection = [
 	{
-		title: 'Menú DOP Nacional',
-		file: 'DOPNavidad21.pdf',
-		type: 'flyer',
-	},
-	{
-		title: 'Invitación Día de la Oportunidad Navidad 2021',
-		file: 'InvitacionDiaOportunidadNavidad2021.pdf',
-		type: 'flyer',
+		name: 'Programa IMSS periodo B 2021',
+		file: 'IMSS_periodoB.pdf',
 	},
 	{
-		title: 'Infografía Gerente de zona',
-		file: 'InfografiaGerenteDeZona.pdf',
-		type: 'flyer',
+		name: 'Programa IMSS periodo A 2021',
+		file: 'flyerA_IMSS.pdf',
 	},
 	{
-		title: 'Infografía Empresaria',
-		file: 'InfografiaEmpresaria.pdf',
-		type: 'flyer',
+		name: 'Flyer IMSS Periodo C 2020',
+		file: 'Flyer_IMSS_Periodo_C_2020.pdf',
+	},
+  ];
+  
+  
+  const vacationsSection = [
+	{
+		name: 'Bases Vacaciones Liderazgo Avon 2020',
+		file: 'Bases_vacaciones_2020.pdf',
+	},
+  ];
+  
+  
+  const videosSection = [
+	{
+		name: 'Jingle Circulo Estelar',
+		file: 'JingleCirculoEstelar.mp4',
 	},
 	{
-		title: 'Infografía Representante',
-		file: 'InfografiaRepresentante.pdf',
-		type: 'flyer',
+		name: 'Ofrece 60/10',
+		file: 'Ofrece_60_10.mp4',
 	},
 	{
-		title: 'Infografía Consumidor',
-		file: 'InfografiaConsumidor.pdf',
-		type: 'flyer',
+		name: 'Coreografía Vacaciones',
+		file: 'CoreografiaVacaciones.mp4',
 	},
 	{
-		title: 'Diseño resumen navidad 2021',
-		file: 'DisenoResumenNavidad2021.pdf',
-		type: 'flyer',
+		name: 'Video Institucional',
+		file: 'VideoInstitucional.mp4',
 	},
 	{
-		title: 'Listado de actividades Día de la Oportunidad',
-		file: 'ListadoActividadesDiaOportunidad.pdf',
-		type: 'flyer',
+		name: '12vo Encuentro de Empresarias',
+		file: '12EncuentroEmpresarias.mp4',
+	},
+  ];
+  const generalToolsSection = [
+	{
+		name: 'Anuncio para prospección en periódicos',
+		file: 'AnuncioLiderazgo.pdf',
 	},
 	{
-		title: 'Presentación ejecución Día de la Oportunidad',
-		file: 'PresentacionEjecucionDOP.pdf',
-		type: 'flyer',
-	},
-
-	{
-		title: 'Guia del Día de la Oportunidad',
-		file: 'GuiaDiaDeLaOportunidad.mp4',
-		type: 'video',
+		name: 'Planograma Actualizado 2020',
+		file: 'Planograma_Actualizado_2020.pdf',
 	},
 	{
-		title: 'Incentivos C19 - C20',
-		file: 'IncentivosC19_20_DOP.mp4',
-		type: 'video',
+		name: 'Guía de Referencia',
+		file: 'GUIA_DE_REFERENCIA_2018-FINAL.pdf',
 	},
 	{
-		title: 'Incentivos C17 - C18',
-		file: 'IncentivosC17_18DOP.mp4',
-		type: 'video',
+		name: 'Acuerdo PM',
+		file: 'Avon_Acuerdo_Empresaria_PM.pdf',
 	},
 	{
-		title: 'Invitación v01',
-		file: 'Invitacion_v01.jpg',
-		type: 'flyer',
+		name: 'Acuerdo TL',
+		file: 'Avon_Acuerdo_Empresaria_TL.pdf',
+	},
+  
+	{
+		name: 'Presentación 60/10',
+		file: 'LIDERAZGO_60_10.pdf',
 	},
 	{
-		title: 'Invitación v02',
-		file: 'Invitacion_v02.jpg',
-		type: 'flyer',
+		name: 'ROA',
+		file: 'Avon_ROA_2017.pdf',
 	},
 	{
-		title: 'Invitación v03',
-		file: 'Invitacion_v03.jpg',
-		type: 'flyer',
+		name: 'Formato de Oportunidad de Ganancia Certificada',
+		file: 'Formato_Oportunidad_Ganancia_Certificada.pdf',
 	},
 	{
-		title: 'Invitación v04',
-		file: 'Invitacion_v04.jpg',
-		type: 'flyer',
+		name: 'Hoja de Retroalimentación',
+		file: 'hoja_retroalimentacion.pdf',
 	},
 	{
-		title: 'Invitación v05',
-		file: 'Invitacion_v05.jpg',
-		type: 'flyer',
+		name: 'Hoja de oportunidad de ganancia',
+		file: 'select',
 	},
-
-  ]
-
-let avDataNCM = [
+  ];
+  const messagesToolsSection = [
 	{
-		title: 'Menú DOP Berilio',
-		file: 'DOPNavidad21Berilio.pdf',
-		type: 'flyer',
-	},
-    {
-		title: 'Infografía Gerente de zona',
-		file: 'InfografiaGerenteDeZonaNCM.pdf',
-		type: 'flyer',
+		name: 'Promesa AVON (video)',
+		file: 'Video_promesa.mp4',
 	},
 	{
-		title: 'Diseño resumen navidad 2021',
-		file: 'DisenoResumenNavidad2021.pdf',
-		type: 'flyer',
+		name: 'Promesa AVON (cartas)',
+		file: 'Carta.zip',
 	},
 	{
-		title: 'Infografía GZ Berilio DOP',
-		file: 'infografiaGZBerilioDOP.pdf',
-		type: 'flyer',
+		name: 'Promesa AVON (infografía)',
+		file: 'Infografia_promesa.jpg',
 	},
 	{
-		title: 'Infografía Consumidor',
-		file: 'InfografiaConsumidor.pdf',
-		type: 'flyer',
+		name: 'Promesa AVON (pines)',
+		file: 'Pines.zip',
 	},
 	{
-		title: 'Guía Día de la oportunidad',
-		file: 'GuiaDOPBerilio2021.mp4',
-		type: 'video',
+		name: 'Promesa AVON (postal)',
+		file: 'Postal_promesa.jpg',
 	},
 	{
-		title: 'Maratón de sorpresas Berilio',
-		file: '360BerilioVideo.mp4',
-		type: 'video',
-	},
-     {
-		title: 'Aparta la fecha',
-		file: 'saveTheDateBerilio.jpg',
-		type: 'flyer',
+		name: 'Promesa AVON (post)',
+		file: 'Post.zip',
 	},
 	{
-		title: 'Invitación v01',
-		file: 'Invitacion_v01.jpg',
-		type: 'flyer',
+		name: 'Promesa AVON (stickers)',
+		file: 'sticker.zip',
 	},
 	{
-		title: 'Invitación v02',
-		file: 'Invitacion_v02.jpg',
-		type: 'flyer',
+		name: 'Dinámica AVON ON',
+		file: 'Dinamica_Avon_On.pdf',
 	},
 	{
-		title: 'Invitación v03',
-		file: 'Invitacion_v03.jpg',
-		type: 'flyer',
+		name: 'Un Millón de Historias (Comunicado)',
+		file: 'Comunicado_1MH_V2.jpg',
 	},
 	{
-		title: 'Invitación v04',
-		file: 'Invitacion_v04.jpg',
-		type: 'flyer',
+		name: 'Un Millón de Historias (Gif)',
+		file: 'Gif_OneMillionStories.gif',
 	},
 	{
-		title: 'Invitación v05',
-		file: 'Invitacion_v05.jpg',
-		type: 'flyer',
+		name: 'Un Millón de Historias (Post Comparte)',
+		file: 'postOMS_Comparte.jpg',
+	},
+	{
+		name: 'Un Millón de Historias (Post Queremos)',
+		file: 'postOMS_Queremos.jpg',
+	},
+	{
+		name: 'Un Millón de Historias (Video Tutorial)',
+		file: 'videotutorialBAJA.mp4',
+	},
+	{
+		name: 'Presentación de Programas Liderazgo convención y Campeonato',
+		file: 'Lanzamiento_CAMPEONAS.pdf',
+	},
+	{
+		name: 'Bases de programa Liderazgo 2021 (convención)',
+		file: 'Bases_Convencion_21.pdf',
+	},
+	{
+		name: 'Bases de programa Liderazgo 2021 (campeonato)',
+		file: 'BASES_CAMPEONATO.pdf',
+	},  
+	{
+		name: 'Nuevo Calendario Liderazgo 2021',
+		file: 'C_Liderazgo2021.pdf',
+	}, 
+	{
+		name: 'Flyer Acompáñame',
+		file: 'C14_Acompaname.pdf',
+	},
+	{
+		name: 'Alta de Cuentas Clabes',
+		file: 'flyerDigital_CuentaClabe.jpg',
+	},
+	{
+		name: 'Cambios en Orden Mínima',
+		file: 'C15_Orden_Minima.pdf',
+	},
+	{
+		name: 'Nuevo Calendario Liderazgo',
+		file: 'N_Calendario_Liderazgo2020.pdf',
+	},
+	{
+		name: 'Apoyo especial Empresarias TL que están por debajo de Ordenes Base',
+		file: 'Bases_de_Apoyo_especial.pdf',
+	},
+	{
+		name: 'Prácticas Comerciales No Aceptadas por Avon ',
+		file: 'practicas_comerciales_no_aceptadas_en_avon.pdf',
+	},
+	{
+		name: 'Actualización de Términos para Empresarias TL',
+		file: 'Avon_actualizacion_terminos_empresarias_TL.pdf',
+	},
+	{
+		name: 'Comunicado: Revisa el Correcto Llenado de tus Nombramientos',
+		file: 'select',
+	},
+  ];
+  const prospectionSection = [
+	{
+		name: 'Fase 1 2021',
+		file: 'fase1_digital.pdf',
+	},
+	{
+		name: 'Fase 2 2021',
+		file: 'fase2_digital.pdf',
+	},
+	{
+		name: 'Anim 1 audio MILLENNIAL',
+		file: 'Anim_1.mp4',
+	},  
+	{
+		name: 'Anim 2 audio GX Y BB',
+		file: 'Anim_2.mp4',
+	},   
+	{
+		name: 'POST 1 GX Y BB',
+		file: 'POST_1_GX.jpg',
+	},   
+	{
+		name: 'POST 1 MILLENNIAL',
+		file: 'POST_1_M.jpg',
+	},   
+	{
+		name: 'POST 2 GX Y BB',
+		file: 'POST_2_GX.jpg',
+	},   
+	{
+		name: 'POST 2 MILLENNIAL',
+		file: 'POST_2_M.jpg',
+	},    
+	 {
+		name: 'POST 3 GX Y BB',
+		file: 'POST_3_GX.jpg',
+	},   
+	{
+		name: 'POST 3 MILLENNIAL',
+		file: 'POST_3_M.jpg',
+	}, 
+	{
+		name: 'POST 4 GX Y BB',
+		file: 'POST_4_GX.jpg',
+	},   
+	{
+		name: 'POST 4 MILLENNIAL',
+		file: 'POST_4_M.jpg',
+	}, 
+		 {
+		name: 'POST 5 GX Y BB',
+		file: 'POST_5_GX.jpg',
+	},   
+	{
+		name: 'POST 5 MILLENNIAL',
+		file: 'POST_5_M.jpg',
+	}, 
+	{
+		name: 'POST 6 GX Y BB',
+		file: 'POST_6_GX.jpg',
+	},   
+	{
+		name: 'POST 6 MILLENNIAL',
+		file: 'POST_6_M.jpg',
+	},    
+	{
+		name: 'POST 7 MILLENNIAL',
+		file: 'POST_7_M.jpg',
+	}, 
+	 {
+		name: 'Video 1 Avon Se Transforma',
+		file: 'Video1.mp4',
+	},
+	 {
+		name: 'Video 2 Avon Se Transforma',
+		file: 'Video2.mp4',
+	},
+	{
+		name: 'ANEW MASKMANIA',
+		file: 'ANEW_MASK.zip',
+	},
+	{
+		name: 'Flyer Inspiracional',
+		file: 'Flyer_Inspiracional.pdf',
+	},
+	{
+		name: 'Plan 60-10 Digital',
+		file: 'Plan_60-10_Digital.pdf',
+	},
+	{
+		name: 'Plan 60-15 Digital',
+		file: 'Plan_60-15_Digital.pdf',
+	},
+	{
+		name: 'Check List ROA',
+		file: 'check_list_roa.pdf',
+	},
+	{
+		name: 'Anuncio Prospección Liderazgo',
+		file: 'AnuncioLiderazgo.pdf',
+	},
+	{
+		name: 'Plan de Actividades de Campo para Empresarias',
+		file: 'avon_gana_mas.pdf',
+	},
+  ];
+  const seasonPropsSection = [
+   {
+		name: 'Certificaciones 2021',
+		file: 'DIPTICO_CERTIFICACIONES.pdf',
+	},   
+   {
+		name: 'IMSS Periodo 2',
+		file: 'IMSS_periodo_2.pdf',
+	}, 
+	{
+		name: 'Plan 60 15 Liderazgo',
+		file: 'Plan_60_15_Liderazgo.pdf',
+	}, 
+  {
+		name: 'Video Circulo Estelar 1',
+		file: 'CE_3er_LOA.mp4',
+	},   
+	{
+		name: 'Video Circulo Estelar 2',
+		file: 'Guia_al_exito_C1.mp4',
+	}, 
+   {
+		name: 'Apoyo de Verano',
+		file: 'FLYER_VERANO_13.pdf',
+	},  
+	{
+		name: 'Flyer Campeonato 2021',
+		file: 'FLYER_CAMPEONATO_2021.pdf',
+	},
+	{
+		name: 'Tri Campeonato 3er Trimestre',
+		file: 'B_3_trimestre_21.pdf',
+	},
+	{
+		name: 'Bases Generales Convención de Ventas Liderazgo Avon 2021',
+		file: 'Bases_Convencion_b.pdf',
+	},
+	{
+		name: 'Bases Generales Campeonas Trimestral Liderazgo Avon 2021',
+		file: 'Bases_Generales_b.pdf',
+	},
+	{
+		name: 'Cruzada de Certificación (flyer)',
+		file: 'cruzada.jpg',
+	},   
+	{
+		name: 'Cruzada de Certificación (video)',
+		file: 'cruzada.mp4',
+	},
+	{
+		name: 'Bases Programa Empresarias Master',
+		file: 'Bases_Empresarias_master.pdf',
+	},
+	{
+		name: 'Bases Cruzada de Certificación',
+		file: 'Bases_cruzada.pdf',
+	}, 
+	{
+		name: 'Simulador Apoyo de Retención C7, 2021',
+		file: 'APOYO_R_C07-21.xlsx',
+	}, 
+	{
+		name: 'Simulador Apoyo de Retención C6, 2021',
+		file: 'APOYO_R_C06-21.xlsx',
+	},
+	{
+		name: 'Simulador Apoyo de Retención C3, 2021',
+		file: 'SIMULADOR_A_C03.xlsx',
+	},
+	{
+		name: 'Diptico 60/15 C3 y C5 2021',
+		file: 'DIPTICO_60_15.pdf',
+	},
+	{
+		name: 'Simulador Apoyo Retención C02, 2021',
+		file: 'SIMULADOR_C02.xlsx',
+	},
+	{
+		name: 'Flyer Apoyo de Retención C2 y C3',
+		file: 'flyerRetencion_C2C3.pdf',
+	},
+  ];
+  const modelPropsSection = [
+	{
+		name:'Sin documentos por el momento',
+		file: ''
+	},
+  ];
+  const celayaSection = [
+	{
+		name:'Ganadoras del viaje a la planta de Celaya fase 1 y certificadas',
+		file: 'Ganadoras_Celaya.pdf'
+	},
+	{
+		name:'Visita Celaya para Empresarias Fase 1',
+		file: 'flyerCelaya_Fase1.pdf'
+	},
+	{
+		name:'Visita Celaya para Empresarias Fase 2 y 3',
+		file: 'flyerCelaya_Fase2y3.pdf'
+	},
+	{
+		name:'Visita Celaya para Empresarias TL',
+		file: 'flyerCelaya_Fase1_40.pdf'
+	},
+  ];
+  const cen4q = [
+	{
+		name:'Apoyo de Certificaciones C16 a C18 60_15',
+		file: 'ApoyodeCertificacionesC16_C18_60_15.pdf'
+	},
+	{
+		name:'Bases Convención B 2021',
+		file: 'Bases_Convencion_b_2021.pdf'
+	},
+	{
+		name:'Flyer Convención',
+		file: 'flyer_Convencion.pdf'
+	},
+	{
+		name:'IMSS Periodo C 2021',
+		file: 'IMSS_periodoC_2021.pdf'
+	},
+	{
+		name:'Plan 60_15 de C16 a C18',
+		file: 'Plan_60_15_C16_C18.pdf'
+	},
+	{
+		name:'Bases Campeonato 4o Trimestre C16 - C20',
+		file: 'Bases_Campeonato_4oTrimestreC16_C20.pdf'
 	},
 	
- 
-  ]
-   
-  
-  $(document).ready(function(){
-
-	var d = AvonAnalyticsObjex.Profile.zone;
-    var dm = AvonAnalyticsObjex.Profile.repAcct;
-	var user = AvonAnalyticsObjex.Content.siteID;
-	var NCMzone = [
-        '2700','2701','2702','2703','2704','2705','2706','2707','2708','2709','2710',
-    ];
-    
-	var brochuresConstructor;
-	var brochurePrint = ["",""];
-	if(user == 'REPSuite' || user == 'SLSuite') {
-		avDataNCM = [avDataNCM.shift()];
-		avData = [avData.shift()];
-		avDataNCM.push(
+  ];
+  var avNCMSL = ['2700','2701','2702','2703','2704','2705','2706','2707','2708','2709','2710'];
+	if (avNCMSL.includes(AvonAnalyticsObjex.Profile.zone)) {} else {
+		if(AvonAnalyticsObjex.Content.siteID == "SLSuite") {
+		seasonPropsSection.unshift(
 			{
-				title: 'Guía Día de la oportunidad',
-				file: 'GuiaDOPBerilio2021.mp4',
-				type: 'video',
-			},
-			{
-				title: 'Maratón de sorpresas Berilio',
-				file: '360BerilioVideo.mp4',
-				type: 'video',
-			},
-			{
-				title: 'Diseño resumen navidad 2021',
-				file: 'DisenoResumenNavidad2021.pdf',
-				type: 'flyer',
-			},
-			{
-				title: 'Infografía Consumidor',
-				file: 'InfografiaConsumidor.pdf',
-				type: 'flyer',
+				name:'Bases Campeonato 4o Trimestre C16 - C20',
+				file: 'Bases_Campeonato_4oTrimestreC16_C20.pdf'
 			},
 		)
-	}
-    if(NCMzone.includes(d) || dm == '888888827') {
-        brochuresConstructor = avDataNCM;
-    } else {
-        brochuresConstructor = avData;
-    }
-	var e = 0;
-	var i;
-	for (i=0;i<brochuresConstructor.length;i++){
-		if(i == 0) {
-			brochurePrint[0] += '<div class="av-brochure-item" data-type="'+ brochuresConstructor[i].type +'">'+
-					'<div class="av-brochure-image" style="background-image:url(\'/ZMSuite/static/pdf/dia_de_la_oportunidad/MenuDiaOportunidadNavidadNacional.pdf.jpg\')">' +
-						'<div class="av-copy av-tooltip"><span class="material-icons">content_copy</span><div class="tooltiptext">Copiar link</div></div>' +
-						'<input type="text" class="av-hidden" value="https://www.mx.avon.com/FLDSuite/static/pdf/mis_folletos/' + brochuresConstructor[i].file + '" />' +
-						'<a class="av-overlay" href="https://www.mx.avon.com/FLDSuite/static/pdf/mis_folletos/' + brochuresConstructor[i].file + '" target="_blank"><p class="material-icons">visibility</p><p>Ver</p></a>' +
-					'</div>' +
-					'<div class="av-brochure-text"><h3 class="av-brochure-title">'+ brochuresConstructor[i].title +'</h3></div>'+
-				'</div>'
-		} else {
-				brochurePrint[0] += '<div class="av-brochure-item" data-type="'+ brochuresConstructor[i].type +'">'+
-				'<div class="av-brochure-image" style="background-image:url(\'/ZMSuite/static/pdf/dia_de_la_oportunidad/' + brochuresConstructor[i].file +'.jpg\')">' +
-					'<div class="av-copy av-tooltip"><span class="material-icons">content_copy</span><div class="tooltiptext">Copiar link</div></div>' +
-					'<input type="text" class="av-hidden" value="https://www.mx.avon.com/ZMSuite/static/pdf/dia_de_la_oportunidad/' + brochuresConstructor[i].file + '" />' +
-					'<a class="av-overlay" href="https://www.mx.avon.com/ZMSuite/static/pdf/dia_de_la_oportunidad/' + brochuresConstructor[i].file + '" target="_blank"><p class="material-icons">visibility</p><p>Ver</p></a>' +
-				'</div>' +
-				'<div class="av-brochure-text"><h3 class="av-brochure-title">'+ brochuresConstructor[i].title +'</h3></div>'+
-			'</div>'
 		}
-		
 	}
-	brochurePrint[1] += '<div class="av-brochure-item av-brochure-main" data-type="'+ brochuresConstructor[0].type +'">'+
-							'<div style="background-image:url(\'/ZMSuite/static/pdf/dia_de_la_oportunidad/MenuDiaOportunidadNavidadNacional.pdf.jpg\')" class="av-brochure-image">' +
-								'<a class="av-overlay" href="https://www.mx.avon.com/FLDSuite/static/pdf/mis_folletos/' + brochuresConstructor[0].file + '" target="_blank"><p class="material-icons">visibility</p><p>Ver</p></a>' +
-							'</div>' +
-							'<div class="av-brochure-text"><h3 class="av-brochure-title">'+ brochuresConstructor[0].title +'</h3></div>' +
-						'</div>'
 
-    try {
-        $('#brochureContainer').prepend(brochurePrint[0]);
-        $('#brochureAside').prepend(brochurePrint[1]);   
-    } catch {
-        $('#brochureContainer').prepend('<h1 style="padding:20px 30px;font-size:37px;line-height: 39px;min-height: 60vh;"><span class="material-icons" style="font-size:65px;margin-bottom:10px;">timer</span><br/>Disculpa las molestias, por el momento no tenemos materiales disponibles<br /><br />Intenta más tarde.</h1>');
-    }
-	
-	avCopy();
+  var avConstructor = [generalToolsSection,messagesToolsSection,prospectionSection,seasonPropsSection,celayaSection,modelPropsSection,videosSection,imssSection,vacationsSection,cen4q];
+  debugger
+  var once = ["true","true","true","true","true","true","true","true","true","true"];
   
-	function avCopy (){
-		$('.av-copy').click(function(){
-			$(this).next().select();
-			document.execCommand("copy");
-			$(this).find('.tooltiptext').text('¡Copiado al portapapeles!');
-			$(this).mouseleave(function(){
-				$(this).find('.tooltiptext').text('Copiar link');  
-			})
-		});
-	};
-  })
+  var e = 0;
+  var i;
   
+  var avArrayPrintContent = ["","","","","","","","","","","","","","","","","","","","","",""];
+  while (e < avConstructor.length){
+	for (i = 0; i < avConstructor[e].length; i++) {
+		avArrayPrintContent[e] += '<li>' +
+			'<span>' + avConstructor[e][i].name + '</span>' +
+			'<span class="av-list-spacer"></span>' +
+			'<a class="av-button-icon" href="/SLSuite/static/liderazgo_2014/descargables/' + avConstructor[e][i].file + '" target="_blank"><span class="material-icons">visibility</span></a>' +
+			'<a download class="av-button-icon" href="/SLSuite/static/liderazgo_2014/descargables/' + avConstructor[e][i].file + '" target="_blank"><span class="material-icons">cloud_download</span></a>'+
+			'</li>';
+	}
+	e++    
+  }
   
